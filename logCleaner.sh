2b37a475dcffb2b3c0603eceb34163964c76c1fc
Manthan997 <manthanparsekar997@gmail.com>
2024-02-05 11:34:16 +0530

    thoda aurr

---
+++ logCleaner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# EXP ID
+# > 4.5 maybe?
+
+# AIM
+# > to clean the damn log files
+
+# CODE
+Log_dir=/var/log
+cd $Log_dir
+
+cat /dev/null > messages
+# notifications?
+
+cat /dev/null > wtmp
+# login details
+
+echo "he he he! your sins have been erased!!"
