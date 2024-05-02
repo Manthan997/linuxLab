662603be88a9f0d7091e3df46f2161cc7b60c67f
dev-69 <72134963+dev-69@users.noreply.github.com>
2024-05-02 13:42:45 +0530

    Add files via upload

---
+++ qb37/20_System_Info.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "SHELL: $SHELL"
+echo "USER: $USER"
+echo "PWD: $PWD"
+echo "PATH: $PATH"
+echo "LANG: $LANG"
+echo "HOME: $HOME"
+echo "BASH_VERSION: $BASH_VERSION"
+
+echo "HOSTNAME: $HOSTNAME"
+echo "UID: $UID"
