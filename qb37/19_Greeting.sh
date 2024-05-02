d7e51dc4297e2cefcc5430f7f13ce62e62dcb799
dev-69 <72134963+dev-69@users.noreply.github.com>
2024-05-02 13:36:52 +0530

    Add files via upload

---
+++ qb37/19_Greeting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define variables
+name="NAME"
+age=21
+country="India"
+
+# Display greeting message with information
+echo "Hello, $name!"
+echo "Nice to meet you. Here's some information about me:"
+echo "Name: $name"
+echo "Age: $age"
+echo "Country: $country"
