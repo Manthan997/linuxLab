34de74f636349101d62b12e254dad1d59bd1dae3
WannabeProdigy123 <145684914+WannabeProdigy123@users.noreply.github.com>
2024-05-04 00:49:15 +0530

    Add files via upload

---
+++ qb37/stringequal.sh
@@ -0,0 +1,9 @@
+string1="hello"
+string2="world"
+
+# Check if the strings are the same
+if [ "$string1" = "$string2" ]; then
+    echo "The strings are the same."
+else
+    echo "The strings are different."
+fi
