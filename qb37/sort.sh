47a1bad6618256b82c7f15dc7ed339c3940ec2c5
Manthan997 <manthanparsekar997@gmail.com>
2024-05-03 20:34:30 +0530

    a3

---
+++ qb37/sort.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# EXP ID
+# > 32
+
+# AIM
+# > Write shell script to demonstrate Sort command with different sort command 
+#-> option: sort –b,sort –r,sort –o,sort –n,sort –M,sort –u,sort -k, sort -t SEP
+
+
+# CODE
+
